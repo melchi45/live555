9d1cdb52ffd45f26fd882e72630059305483c4db
melchi45 <melchi45@gmail.com>
2024-07-09 17:41:33 +0900

    [origin] Bump up to 2024.06.26 [113.3.1]
    (cherry picked from commit 4ac938c8e935a60c80b37a527bc5b9d738ea4a3f)

--- liveMedia/include/liveMedia_version.hh
+++ liveMedia/include/liveMedia_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _LIVEMEDIA_VERSION_HH
 #define _LIVEMEDIA_VERSION_HH
 
-#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2024.05.30"
-#define LIVEMEDIA_LIBRARY_VERSION_INT		1717027200
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2024.06.26"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1719360000
 
 extern char const* const liveMediaLibraryVersionStr;
 extern int const liveMediaLibraryVersionInt;
