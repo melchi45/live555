e2661c0ae462e48b1b7d2cf10c34d980b24e9559
melchi45 <melchi45@gmail.com>
2024-08-23 18:20:18 +0900

    [origin] Bump up to 2024.08.01 [113.4.1]

--- groupsock/include/groupsock_version.hh
+++ groupsock/include/groupsock_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _GROUPSOCK_VERSION_HH
 #define _GROUPSOCK_VERSION_HH
 
-#define GROUPSOCK_LIBRARY_VERSION_STRING	"2024.06.26"
-#define GROUPSOCK_LIBRARY_VERSION_INT		1719360000
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2024.08.01"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1722470400
 
 extern char const* const groupsockLibraryVersionStr;
 extern int const groupsockLibraryVersionInt;
