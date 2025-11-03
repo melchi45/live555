dc905a7c5a6dbdc76182d5262befbb591f6800d4
melchi45 <melchi45@gmail.com>
2025-11-03 15:32:51 +0900

    [origin] Bump up to 2025.10.13 [117.0.0]

--- liveMedia/include/RTSPServer.hh
+++ liveMedia/include/RTSPServer.hh
@@ -131,16 +131,11 @@ protected:
       // used to implement "RTSPClientConnection::handleCmd_REGISTER()"
 
   virtual UserAuthenticationDatabase* getAuthenticationDatabaseForCommand(char const* cmdName);
-  virtual Boolean specialClientAccessCheck(int clientSocket,
-					   struct sockaddr_storage const& clientAddr,
-					   char const* urlSuffix);
-      // a hook that allows subclassed servers to do server-specific access checking
-      // on each client (e.g., based on client IP address), without using digest authentication.
   virtual Boolean specialClientUserAccessCheck(int clientSocket,
 					       struct sockaddr_storage const& clientAddr,
 					       char const* urlSuffix, char const *username);
-      // another hook that allows subclassed servers to do server-specific access checking
-      // - this time after normal digest authentication has already taken place (and would otherwise allow access).
+      // a hook that allows subclassed servers to do server-specific access checking - after
+      // normal digest authentication has already taken place (and would otherwise allow access).
       // (This test can only be used to further restrict access, not to grant additional access.)
   virtual void specialHandlingOfAuthenticationFailure(int clientSocket,
 						      struct sockaddr_storage const& clientAddr,
@@ -219,7 +214,7 @@ public: // should be protected, but some old compilers complain otherwise
     void closeSocketsRTSP();
     static void handleAlternativeRequestByte(void*, u_int8_t requestByte);
     void handleAlternativeRequestByte1(u_int8_t requestByte);
-    Boolean authenticationOK(char const* cmdName, char const* urlSuffix, char const* fullRequestStr);
+    virtual Boolean authenticationOK(char const* cmdName, char const* urlSuffix, char const* fullRequestStr);
     void changeClientInputSocket(int newSocketNum, ServerTLSState const* newTLSState,
 				 unsigned char const* extraData, unsigned extraDataSize);
       // used to implement RTSP-over-HTTP tunneling
--- liveMedia/include/liveMedia_version.hh
+++ liveMedia/include/liveMedia_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _LIVEMEDIA_VERSION_HH
 #define _LIVEMEDIA_VERSION_HH
 
-#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2025.07.10"
-#define LIVEMEDIA_LIBRARY_VERSION_INT		1752105600
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2025.10.13"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1760313600
 
 extern char const* const liveMediaLibraryVersionStr;
 extern int const liveMediaLibraryVersionInt;
