ce3ee00104995e22f63ea66480214fd81717ac4b
melchi45 <melchi45@gmail.com>
2026-05-29 04:35:33 +0000

    Update from remote archive 2026-05-29
    (cherry picked from commit 80165a8af1dad923317adac37f549e416e9ea5ba)

--- BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
+++ BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
 #define _BASICUSAGEENVIRONMENT_VERSION_HH
 
-#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2026.04.22"
-#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1776816000
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2026.05.29"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1780012800
 
 extern char const* const BasicUsageEnvironmentLibraryVersionStr;
 extern int const BasicUsageEnvironmentLibraryVersionInt;
