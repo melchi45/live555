bd2d36470b704aed18c9ebe245f81828f7c86135
melchi45 <melchi45@gmail.com>
2026-06-30 04:31:49 +0000

    Update from remote archive 2026-06-30
    (cherry picked from commit 8a995b844e5e773ec818a469ae87d2ddfeb2bb04)

--- groupsock/include/groupsock_version.hh
+++ groupsock/include/groupsock_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _GROUPSOCK_VERSION_HH
 #define _GROUPSOCK_VERSION_HH
 
-#define GROUPSOCK_LIBRARY_VERSION_STRING	"2026.06.24"
-#define GROUPSOCK_LIBRARY_VERSION_INT		1782259200
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2026.06.29"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1782691200
 
 extern char const* const groupsockLibraryVersionStr;
 extern int const groupsockLibraryVersionInt;
