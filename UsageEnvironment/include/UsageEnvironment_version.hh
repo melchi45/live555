59981e4e055c4d58a3e191ab9ed7989820172be8
melchi45 <melchi45@gmail.com>
2026-07-08 06:04:06 +0000

    Update from remote archive 2026-07-08

--- UsageEnvironment/include/UsageEnvironment_version.hh
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _USAGEENVIRONMENT_VERSION_HH
 #define _USAGEENVIRONMENT_VERSION_HH
 
-#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2026.07.06"
-#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1783296000
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2026.07.08"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1783468800
 
 extern char const* const UsageEnvironmentLibraryVersionStr;
 extern int const UsageEnvironmentLibraryVersionInt;
