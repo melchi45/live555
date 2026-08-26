17ab96abf20c5bcdf918bdeeafbd2f2369f5b05e
melchi45 <melchi45@gmail.com>
2026-08-26 03:57:38 +0000

    Update from remote archive 2026-08-26
    (cherry picked from commit f85e0a49cbd889129786a884459113a700bc9725)

--- BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
+++ BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
 #define _BASICUSAGEENVIRONMENT_VERSION_HH
 
-#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2026.08.14"
-#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1786665600
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2026.08.25"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1787616000
 
 extern char const* const BasicUsageEnvironmentLibraryVersionStr;
 extern int const BasicUsageEnvironmentLibraryVersionInt;
--- liveMedia/include/liveMedia_version.hh
+++ liveMedia/include/liveMedia_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _LIVEMEDIA_VERSION_HH
 #define _LIVEMEDIA_VERSION_HH
 
-#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2026.08.14"
-#define LIVEMEDIA_LIBRARY_VERSION_INT		1786665600
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2026.08.25"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1787616000
 
 extern char const* const liveMediaLibraryVersionStr;
 extern int const liveMediaLibraryVersionInt;
